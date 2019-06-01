861313f20aa7fd7bfb345a82e4611a5b34e2a384
Samuel Cole <cole.samuel21@gmail.com>
2019-06-01 08:49:37 -0500

    Add more backend boilerplate

---
+++ backend/src/entity/User.ts
@@ -0,0 +1,25 @@
+import {
+  Entity,
+  BaseEntity,
+  PrimaryColumn,
+  Column,
+  BeforeInsert
+} from "typeorm";
+import * as uuid from "uuid/v4";
+
+@Entity("users")
+export class User extends BaseEntity {
+  @PrimaryColumn("uuid")
+  public id: string;
+
+  @Column("varchar", { length: 255 })
+  public email: string;
+
+  @Column("text")
+  public password: string;
+
+  @BeforeInsert()
+  public addId() {
+    this.id = uuid();
+  }
+}
