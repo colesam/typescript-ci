6839fd70bbde38567cc7d009f32d074819f19d22
Samuel Cole <cole.samuel21@gmail.com>
2019-06-01 08:38:40 -0500

    Add in boilerplate files for backend

---
+++ backend/ormconfig.js
@@ -0,0 +1,20 @@
+import * as process from "process";
+
+module.exports = {
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  synchronize: false,
+  logging: false, // turn on for SQL logs
+  entities: ["src/entity/**/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
+  subscribers: ["src/subscriber/**/*.ts"],
+  cli: {
+    entitiesDir: "src/entity",
+    migrationsDir: "src/migration",
+    subscribersDir: "src/subscriber"
+  }
+};
